9ec18f755623e14cd4c1186f06686ca07c0dc7c1
lucacesana02 <93332890+lucacesana02@users.noreply.github.com>
2021-12-17 19:36:10 +0100

    Add files via upload

--- triangolo.c
+++ triangolo.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-int main(void) {
-    float ax,ay,bx,by,cx,cy;
-    float lato1, lato2, lato3;
-    float perimetro;
-    float m,q;
+// S = radice quadrata(p*(p-l1)*(p-l2)*(p-l3))
+//l1, l2, l3 = lati
+//p = semiperimetro
+
+//Calcolare la radice quadrata intera di un numero
+
+int main(int argc, const char * argv[]) {
+    float l1,l2,l3;
+    float p, S;
     
-    printf("Inserisci le coordinate del punto A:\n");
-    scanf("%f %f",&ax,&ay);
-    printf("Inserisci le coordinate del punto B:\n");
-    scanf("%f %f",&bx,&by);
-    printf("Inserisci le coordinate del punto C:\n");
-    scanf("%f %f",&cx,&cy);
+    scanf("%f",&l1);
+    scanf("%f",&l2);
+    scanf("%f",&l3);
     
-    //controllo se il triangolo è degenere
-    //1. i punti non coincidono
-    if (((ax==bx) && (ay==by)) || ((ax==cx) && (ay==cy)) || ((cx==bx) && (cy==by)))
-    {
-        //Almeno due punti coincidono, quindi il triangolo non esiste
-        printf("Il trinagolo non esiste\n");
-        return -1;
+    if ((l1 >=0) &&
+        (l2 >=0) &&
+        (l3 >=0)){
+        if ((l1+l2>=l3) &&
+            (l2+l3>=l1) &&
+            (l1+l3>=l2)){
+            
+            if ((l1+l2==l3) || (l2+l3==l1) || (l1+l3==l2)){
+                printf("Il triangolo è degenere, area = 0\n");
+            }
+            else{
+                //è un triangolo!!!
+                p = (l1+l2+l3)/2;
+                S = sqrt(p*(p-l1)*(p-l2)*(p-l3));
+                
+                printf("L'area vale: %.2f\n",S);
+            }
+        }
+        else{
+            printf("Il triangolo non esiste\n");
+        }
     }
-    //2. i punti non sono allineati
-    //calcolo retta per due punti
-    m = (ay-by)/(ax-bx);
-    q = (ax*by - bx*ay)/(ax-bx);
-    if (cy == m*cx+q)
-    {
-        //I punti sono allineati, ovvero sulla stessa retta
-        printf("I tre punti sono allineati. Il trinagolo non esiste\n");
-        return -1;
-    }
+    else
+        printf("Hai inserito dei lati <0\n");
     
-    //Calcolo i lati
-    lato1 = sqrt(pow(ax-bx,2)+pow(ay-by,2));
-    lato2 = sqrt(pow(bx-cx,2)+pow(by-cy,2));
-    lato3 = sqrt(pow(ax-cx,2)+pow(ay-cy,2));
-    perimetro = lato1+lato2+lato3;
-    
-    printf("Il perimetro vale: %.2f\n",perimetro);
     return 0;
 }
-
